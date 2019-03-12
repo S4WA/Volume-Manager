7f1b7f4fefb1ae2115e643ec1da9ddd4862470e3
S4WA <s4wa@outlook.com>
2019-03-13 08:48:55 +0900

    ...

---
+++ src/bg.js
@@ -0,0 +1,16 @@
+setInterval(function() {
+	chrome.storage.sync.get(null, function(items) {
+		items = Object.keys(items);
+		for (var i = 0; i < items.length; i++) {
+			chrome.tabs.query({
+				url: items[i]
+			}, function (result) {
+				for (var i = 0; i < result.length; i++) {
+					if (!result[i].mutedInfo.muted) {
+						
+					}
+				}
+			});
+		}
+	});
+}, 500);
